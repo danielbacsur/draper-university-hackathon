5b59c644323bccadbf3d6f1d060a109cee9204ac
Daniel Bacsur <contact@danielbacsur.com>
2024-07-13 06:52:20 -0700

    feat: add section-header.tsx and hero section to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,10 @@
 "use client";
 
+import { MeasuredPageWrapper } from "@/components/measured-page";
 import { ScrollControls, Scroll } from "@react-three/drei";
-import { Suspense, useState, useEffect } from "react";
+import { Suspense, useState, useEffect,  } from "react";
 import { Canvas } from "@react-three/fiber";
 import ReactDOM from "react-dom";
-
-function Content() {
-  return (
-    <>
-      <div className="h-screen w-screen grid place-items-center bg-red-500 text-4xl">
-        Red
-      </div>
-      <div className="h-screen w-screen grid place-items-center bg-blue-500 text-4xl">
-        Blue
-      </div>
-      <div className="h-screen w-screen grid place-items-center bg-green-500 text-4xl">
-        Green
-      </div>
-    </>
-  );
-}
 
 export default function OffScreenHeightMeasurer() {
   const [pages, setPages] = useState(0);
@@ -31,7 +16,7 @@ export default function OffScreenHeightMeasurer() {
     offScreenDiv.style.width = "100%";
     document.body.appendChild(offScreenDiv);
 
-    ReactDOM.render(<Content  />, offScreenDiv, () => {
+    ReactDOM.render(<MeasuredPageWrapper />, offScreenDiv, () => {
       setPages(offScreenDiv.offsetHeight / window.innerHeight);
       ReactDOM.unmountComponentAtNode(offScreenDiv);
       document.body.removeChild(offScreenDiv);
@@ -42,16 +27,16 @@ export default function OffScreenHeightMeasurer() {
     <div className="w-full h-screen">
       <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
         <Suspense fallback={null}>
-          <ScrollControls infinite damping={0.1} pages={pages + 1} distance={1}>
+          <ScrollControls infinite damping={0.2} pages={pages + 1} distance={1}>
             <Scroll html>
               <div className="absolute -translate-y-full">
-                <Content />
+                <MeasuredPageWrapper />
               </div>
               <div className="absolute">
-                <Content />
+                <MeasuredPageWrapper />
               </div>
               <div className="absolute translate-y-full">
-                <Content />
+                <MeasuredPageWrapper />
               </div>
             </Scroll>
           </ScrollControls>

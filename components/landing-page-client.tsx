0e83973115ea3b5578fd58dfd898d97b14b1c306
Daniel Bacsur <contact@danielbacsur.com>
2024-07-14 23:30:12 +0200

    feat: refactor landing page and measured page components

--- components/landing-page-client.tsx
+++ components/landing-page-client.tsx
@@ -1,92 +1,55 @@
 "use client";
 
-import { MeasuredPageWrapper } from "@/components/measured-page";
-import { ScrollControls, Scroll } from "@react-three/drei";
-import { Suspense, useState, useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
-import ReactDOM from "react-dom";
-import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { MeasuredPage } from "@/components/measured-page";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { Session } from "next-auth";
 import Link from "next/link";
 
 export function LandingPageClient({ session }: { session: Session | null }) {
-  const [pages, setPages] = useState(0);
-
-  useEffect(() => {
-    const offScreenDiv = document.createElement("div");
-    offScreenDiv.style.position = "absolute";
-    offScreenDiv.style.left = "-9999px";
-    offScreenDiv.style.width = "100%";
-    document.body.appendChild(offScreenDiv);
-
-    ReactDOM.render(<MeasuredPageWrapper />, offScreenDiv, () => {
-      setPages(offScreenDiv.offsetHeight / window.innerHeight);
-      ReactDOM.unmountComponentAtNode(offScreenDiv);
-      document.body.removeChild(offScreenDiv);
-    });
-  }, []);
-
   return (
-    <>
-      <div className="fixed top-12 left-12">
+    <main className="pb-32">
+      <div className="hidden lg:block fixed top-12 left-12">
         <Image src="/logo.png" alt="Logo" width={48} height={48} />
       </div>
+
       <div className="hidden lg:flex fixed top-12 right-12 text-left text-xs leading-normal z-10 space-x-6 items-center">
         <Button variant="link" size="sm" className="px-0" asChild>
           <Link href="/marketplace">Marketplace</Link>
         </Button>
-        <Avatar>
-          <AvatarImage
-            src={session?.user?.image || ""}
-            alt={session?.user?.name
-              ?.split(" ")
-              .slice(0, 2)
-              .map((name) => name[0])
-              .join("")}
-          />
-          <AvatarFallback>
-            {session?.user?.name
-              ?.split(" ")
-              .slice(0, 2)
-              .map((name) => name[0])
-              .join("")}
-          </AvatarFallback>
-        </Avatar>
+        {session?.user && (
+          <Avatar className="hidden md:block">
+            <AvatarImage
+              src={session?.user?.image || ""}
+              alt={session?.user?.name
+                ?.split(" ")
+                .slice(0, 2)
+                .map((name) => name[0])
+                .join("")}
+            />
+            <AvatarFallback>
+              {session?.user?.name
+                ?.split(" ")
+                .slice(0, 2)
+                .map((name) => name[0])
+                .join("")}
+            </AvatarFallback>
+          </Avatar>
+        )}
       </div>
-      <div className="hidden lg:block fixed bottom-12 left-12 text-left text-xs leading-normal z-10">
+
+      <div className="hidden xl:block fixed bottom-12 left-12 text-left text-xs leading-normal z-10">
         <div className="font-bold">Decentralized AI for Everyone</div>
         <div>building a cheaper and better future_</div>
       </div>
-      <div className="hidden lg:block fixed bottom-12 right-12 text-right text-xs leading-normal z-10">
+
+      <div className="hidden xl:block fixed bottom-12 right-12 text-right text-xs leading-normal z-10">
         <div className="font-bold">All rights reserved &copy; 2024</div>
         <div>&gt; in collaboration with @huggingface</div>
       </div>
-      <div className="w-full h-screen">
-        <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
-          <Suspense fallback={null}>
-            <ScrollControls
-              infinite
-              damping={0.2}
-              pages={pages + 1}
-              distance={1}
-            >
-              <Scroll html>
-                <div className="absolute -translate-y-full">
-                  <MeasuredPageWrapper />
-                </div>
-                <div className="absolute">
-                  <MeasuredPageWrapper />
-                </div>
-                <div className="absolute translate-y-full">
-                  <MeasuredPageWrapper />
-                </div>
-              </Scroll>
-            </ScrollControls>
-          </Suspense>
-        </Canvas>
-      </div>
-    </>
+
+      <MeasuredPage />
+    </main>
   );
 }
--- components/measured-page.tsx
+++ components/measured-page.tsx
@@ -8,43 +8,128 @@ import {
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { ViralLoops } from "./viral-loops";
 import { signInWithGoogle } from "@/actions/auth";
+import { Balancer } from "react-wrap-balancer";
+import Image from "next/image";
 
 export function MeasuredPage() {
   return (
-    <>
-      <main>
-        <section className="min-h-screen  grid place-items-center">
-          <PageHeader>
-            <PageHeaderHeading>Trade & Invest In AI Models</PageHeaderHeading>
-            <PageHeaderDescription className="font-sans">
-              Tokenize AI models, enable investors to acquire model shares, and
-              distribute equity based on usage. Deploy and trade
-              HuggingFace&trade; models in minutes and start earning.
-            </PageHeaderDescription>
-            <PageActions>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button>Join Now</Button>
-                </DialogTrigger>
-                <DialogContent className="p-0 bg-transparent border-none">
-                  <ViralLoops ucid="acpGNiK3NioYynVD71NEwqc71sU" />
-                </DialogContent>
-              </Dialog>
-              <Button variant="outline" onClick={() => signInWithGoogle()}>
-                Get Started
-              </Button>
-            </PageActions>
-          </PageHeader>
-        </section>
-      </main>
-    </>
-  );
-}
+    <main className="w-full relative px-4 sm:px-8">
+      <section className="min-h-screen px-4 grid place-items-center relative mb-[-180px] lg:mb-0">
+        <div className="hidden lg:block absolute top-0 left-0 w-full h-full pointer-events-none">
+          <img
+            src="/over.svg"
+            alt="Background"
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <div className="block lg:hidden absolute top-[18vh] -translate-y-1/2 left-1/2 -translate-x-1/2">
+          <Image src="/logo.png" alt="Logo" width={72} height={72} />
+        </div>
+        <PageHeader>
+          <PageHeaderHeading>Trade & Invest in AI Models</PageHeaderHeading>
+          <PageHeaderDescription className="font-sans">
+            Tokenize AI models, enable investors to acquire model shares, and
+            distribute equity based on usage. Deploy and trade
+            HuggingFace&trade; models in minutes and start earning.
+          </PageHeaderDescription>
+          <PageActions>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button>Join the Waitlist</Button>
+              </DialogTrigger>
+              <DialogContent className="p-0 bg-transparent border-none">
+                <ViralLoops ucid="xjpxAiuZ4LAcgVvtt6pRtUvYzZk" />
+              </DialogContent>
+            </Dialog>
+            <Button variant="outline" onClick={() => signInWithGoogle()}>
+              Get Started
+            </Button>
+          </PageActions>
+        </PageHeader>
+      </section>
 
-export function MeasuredPageWrapper() {
-  return (
-    <div className="w-screen relative">
-      <MeasuredPage />
-    </div>
+      <section className="py-10 xl:py-20">
+        <div className="container mx-auto max-w-full lg:max-w-[60vw] px-4 xl:px-12 grid grid-cols-1 xl:grid-cols-2 grid-rows-2 xl:grid-rows-1">
+          <div className="space-y-3">
+            <div className="text-sm">Key Features</div>
+            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl">
+              <Balancer>
+                Unlock Cost Savings with Decentralized AI Solutions
+              </Balancer>
+            </h2>
+
+            <p className="text-muted-foreground xl:text-xl/relaxed text-justify">
+              AiQuity empowers you to tokenize AI models, enabling investors to
+              acquire model shares and distribute equity based on usage. Access
+              Hugging Face's vast AI model library, revolutionizing the way you
+              leverage AI technology.
+            </p>
+          </div>
+
+          <div className="grid place-items-center">
+            <img
+              src="/piggy.svg"
+              alt="Piggy Bank"
+              className="w-2/3 max-w-[250px]"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="py-10 xl:py-20">
+        <div className="container mx-auto max-w-full lg:max-w-[60vw] px-4 xl:px-12 grid grid-cols-1 xl:grid-cols-2 grid-rows-2 xl:grid-rows-1">
+          <div className="space-y-3 order-1 xl:order-2">
+            <div className="text-sm">Key Features</div>
+            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl">
+              <Balancer>Maximize Your Returns with AI Investments</Balancer>
+            </h2>
+
+            <p className="text-muted-foreground xl:text-xl/relaxed text-justify">
+              At AiQuity, investors can acquire shares in cutting-edge AI models
+              and earn equity based on usage. Our platform empowers you to
+              invest in the future of AI technology, offering attractive returns
+              and Bitcoin rewards through a secure, decentralized network.
+            </p>
+          </div>
+
+          <div className="grid place-items-center order-2 xl:order-1">
+            <img
+              src="/whale.svg"
+              alt="Piggy Bank"
+              className="w-2/3 max-w-[250px]"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="py-10 xl:py-20">
+        <div className="container mx-auto max-w-full lg:max-w-[60vw] px-4 xl:px-12 grid grid-cols-1 xl:grid-cols-2 grid-rows-2 xl:grid-rows-1">
+          <div className="space-y-3">
+            <div className="text-sm">Key Features</div>
+            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl">
+              <Balancer>
+                Monetize Your GPUs with Decentralized AI Mining
+              </Balancer>
+            </h2>
+
+            <p className="text-muted-foreground xl:text-xl/relaxed text-justify">
+              Put your idle GPUs to work and earn money with AiQuity. Join our
+              decentralized network, support AI model operations, and receive
+              Bitcoin rewards. By leveraging the collective power of user GPUs,
+              AiQuity ensures efficient, transparent, and secure processing,
+              benefiting AI creators and investors alike.
+            </p>
+          </div>
+
+          <div className="grid place-items-center">
+            <img
+              src="/monke.svg"
+              alt="Piggy Bank"
+              className="w-2/3 max-w-[250px]"
+            />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }

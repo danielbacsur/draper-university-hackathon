ca112d5aa24bc1153b1fd2700ad29132c119aedc
Daniel Bacsur <contact@danielbacsur.com>
2024-07-13 20:00:44 -0700

    feat: fetch user data when loading landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,8 @@
-"use client";
+import { auth } from "@/auth";
+import { LandingPageClient } from "@/components/landing-page-client";
 
-import { MeasuredPageWrapper } from "@/components/measured-page";
-import { ScrollControls, Scroll } from "@react-three/drei";
-import { Suspense, useState, useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
-import ReactDOM from "react-dom";
-import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Image from "next/image";
+export default async function LandingPage() {
+  const session = await auth();
 
-export default function OffScreenHeightMeasurer() {
-  const [pages, setPages] = useState(0);
-
-  useEffect(() => {
-    const offScreenDiv = document.createElement("div");
-    offScreenDiv.style.position = "absolute";
-    offScreenDiv.style.left = "-9999px";
-    offScreenDiv.style.width = "100%";
-    document.body.appendChild(offScreenDiv);
-
-    ReactDOM.render(<MeasuredPageWrapper />, offScreenDiv, () => {
-      setPages(offScreenDiv.offsetHeight / window.innerHeight);
-      ReactDOM.unmountComponentAtNode(offScreenDiv);
-      document.body.removeChild(offScreenDiv);
-    });
-  }, []);
-
-  return (
-    <>
-      <div className="fixed top-12 left-12">
-        <Image src="/logo.png" alt="Logo" width={48} height={48} />
-      </div>
-      <div className="hidden lg:flex fixed top-12 right-12 text-left text-xs leading-normal z-10 space-x-6 items-center">
-        <Button variant="link" size="sm" className="px-0">Marketplace</Button>
-        <Button variant="link" size="sm" className="px-0">Account</Button>
-        <Avatar>
-          <AvatarImage src="https://github.com/danielbacsur.png" alt="@shadcn" />
-          <AvatarFallback>DB</AvatarFallback>
-        </Avatar>
-      </div>
-      <div className="hidden lg:block fixed bottom-12 left-12 text-left text-xs leading-normal z-10">
-        <div className="font-bold">Decentralized AI for Everyone</div>
-        <div>building a cheaper and better future_</div>
-      </div>
-      <div className="hidden lg:block fixed bottom-12 right-12 text-right text-xs leading-normal z-10">
-        <div className="font-bold">Get In Touch</div>
-        <div>&gt; in collaboration with @huggingface</div>
-      </div>
-      <div className="w-full h-screen">
-        <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
-          <Suspense fallback={null}>
-            <ScrollControls
-              infinite
-              damping={0.2}
-              pages={pages + 1}
-              distance={1}
-            >
-              <Scroll html>
-                <div className="absolute -translate-y-full">
-                  <MeasuredPageWrapper />
-                </div>
-                <div className="absolute">
-                  <MeasuredPageWrapper />
-                </div>
-                <div className="absolute translate-y-full">
-                  <MeasuredPageWrapper />
-                </div>
-              </Scroll>
-            </ScrollControls>
-          </Suspense>
-        </Canvas>
-      </div>
-    </>
-  );
+  return <LandingPageClient session={session} />;
 }

13196c1c290c342edaa9c105a83ec76ed52df0a8
Daniel Bacsur <contact@danielbacsur.com>
2024-07-15 09:50:59 +0200

    fix: remove hard coded huggingface environment variable

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -1,21 +1,21 @@
-import { HuggingFaceStream, StreamingTextResponse } from 'ai';
-import { HfInference } from '@huggingface/inference';
+import { HuggingFaceStream, StreamingTextResponse } from "ai";
+import { HfInference } from "@huggingface/inference";
 
-const Hf = new HfInference("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");
+const huggingface = new HfInference(process.env.HUGGINGFACE_API_KEY);
 
 export async function POST(req: Request) {
   const { prompt, model, hello } = await req.json();
 
-  const response = await Hf.textGenerationStream({
-    model: model || 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
+  const response = await huggingface.textGenerationStream({
+    model: model || "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
     inputs: `<|prompter|>${prompt}<|endoftext|><|assistant|>`,
     parameters: {
       max_new_tokens: 200,
       typical_p: 0.2,
       repetition_penalty: 1,
       truncate: 1000,
-      return_full_text: false
-    }
+      return_full_text: false,
+    },
   });
 
   const stream = HuggingFaceStream(response);
